f3f9c06270339ed47b2f21cc004a4ff186836a1f
t-richmond <trichmond@deloitte.com.au>
2024-05-14 09:47:58 +1000

    Custom highlighter and colour change plugin for DatoCMS

--- src/entrypoints/ConfigScreen.tsx
+++ src/entrypoints/ConfigScreen.tsx
@@ -1,15 +1,73 @@
-import type { RenderConfigScreenCtx } from 'datocms-plugin-sdk';
-import { Canvas, ContextInspector } from 'datocms-react-ui';
-import s from './styles.module.css';
+import type { RenderConfigScreenCtx } from "datocms-plugin-sdk";
+import { Button, Canvas, ContextInspector, TextField } from "datocms-react-ui";
+import s from "./styles.module.css";
+import { useState } from "react";
+import { Spinner } from "datocms-react-ui";
+import { rgb, parseToRgb } from "polished";
 
 type Props = {
   ctx: RenderConfigScreenCtx;
 };
 
 export default function ConfigScreen({ ctx }: Props) {
+  const parameters = ctx.plugin.attributes.parameters.colours as string;
+  const [colours, setColours] = useState(parameters);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  const updateColours = async () => {
+    setIsLoading(true);
+    setError("");
+
+    //* Ensure hex codes are valid.
+    try {
+      if (colours) {
+        colours.split(",").map((color) => rgb(parseToRgb(color.trim())));
+        await ctx
+          .updatePluginParameters({ colours: colours })
+          .then(() => {
+            ctx.notice("Colours updated successfully.");
+            setIsLoading(false);
+          })
+          .catch(() => {
+            ctx.alert("Error updating colours, please try again.");
+            setIsLoading(false);
+          });
+      }
+    } catch (e) {
+      setError(
+        "Invalid color string. Make sure to pass a comma seperated list of valid hex colors"
+      );
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Canvas ctx={ctx}>
-      <p>Welcome to your plugin! This is your config screen!</p>
+      <TextField
+        name="colours"
+        id="colours"
+        error={error}
+        label="Custom colours"
+        hint="List colours that you would like to customise in Structured Text. Delimit colors with a comma."
+        placeholder="#FF0000, #00FF00, #0000FF"
+        value={colours}
+        onChange={(changeValue) => {
+          setColours(changeValue);
+        }}
+      />
+      <Button
+        style={{ marginTop: "16px" }}
+        fullWidth
+        type="submit"
+        buttonType="primary"
+        rightIcon={isLoading ? <Spinner size={20} /> : ""}
+        onClick={async () => {
+          await updateColours();
+        }}>
+        Save colours
+      </Button>
+
       <div className={s.inspector}>
         <ContextInspector />
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,69 @@
-import { connect } from 'datocms-plugin-sdk';
-import 'datocms-react-ui/styles.css';
-import ConfigScreen from './entrypoints/ConfigScreen';
-import { render } from './utils/render';
+import {
+  connect,
+  Field,
+  FieldIntentCtx,
+  StructuredTextCustomBlockStyle,
+} from "datocms-plugin-sdk";
+import "datocms-react-ui/styles.css";
+import ConfigScreen from "./entrypoints/ConfigScreen";
+import { render } from "./utils/render";
+import { GetColorName } from "hex-color-to-color-name";
 
 connect({
   renderConfigScreen(ctx) {
     return render(<ConfigScreen ctx={ctx} />);
+  },
+
+  customMarksForStructuredTextField(field: Field, ctx: FieldIntentCtx) {
+    return [
+      {
+        id: "colour-emphasis",
+        label: "Text Colour",
+        icon: "paint-brush",
+        keyboardShortcut: "mod+shift+p",
+        appliedStyle: {
+          backgroundColor: "rgba(255, 0, 0, 0.3)",
+        },
+      },
+    ];
+  },
+
+  customBlockStylesForStructuredTextField(field: Field, ctx: FieldIntentCtx) {
+    const coloursReference = ctx.plugin.attributes.parameters.colours as string;
+    const coloursArray = coloursReference.split(",");
+    const renderColourOptions: StructuredTextCustomBlockStyle[] = [];
+
+    //* Break and return if there are no colours in ctx params
+    if (!coloursReference) {
+      return [];
+    }
+
+    //* Heading colours
+    coloursArray.forEach((colour: string) => {
+      const colourTrimmed = colour.trim();
+      renderColourOptions.push({
+        id: colourTrimmed,
+        node: "heading",
+        label: GetColorName(colourTrimmed),
+        appliedStyle: {
+          color: colourTrimmed,
+        },
+      });
+    });
+
+    //* Paragraph colours
+    coloursArray.forEach((colour: string) => {
+      const colourTrimmed = colour.trim();
+      renderColourOptions.push({
+        id: colourTrimmed,
+        node: "paragraph",
+        label: GetColorName(colourTrimmed),
+        appliedStyle: {
+          color: colourTrimmed,
+        },
+      });
+    });
+
+    return renderColourOptions;
   },
 });
